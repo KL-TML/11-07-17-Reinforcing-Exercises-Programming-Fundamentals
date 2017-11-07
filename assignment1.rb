a4e36f412106cc90818e51f8ea000965382b9ee6
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-07 15:39:54 -0500

    assgnmt1 change

--- assignment1.rb
+++ assignment1.rb
@@ -1,21 +1,12 @@
 # Assignment 1
 
-# Exercise 1
-emotions = {
-  :happiness => 1,
-  :sadness => 2,
-  :excitment => 3,
-  :anxiousness => 1,
-  :sleepyness => 2
-  }
-
 
 # Exercise 2
 class Person
 
-  def initialize(name, emotion_level)
+  def initialize(name)
     @name = name
-    @emotions = emotion_level
+    @emotions = {happiness: rand(1..3), sadness: rand(1..3), excitment: rand(1..3)}
   end
 
   def name
@@ -30,11 +21,11 @@ class Person
 
   def emotion_metre
     emotions.each do |emoji, value|
-      if value == 3
+      if value == 1
         puts "#{name} is feeling a high amount of #{emoji}"
       elsif value == 2
         puts "#{name} is feeling a medium amount of #{emoji}"
-      else value == 1
+      else value == 3
         puts "#{name} is feeling a low amount of #{emoji}"
       end
     end
@@ -43,6 +34,6 @@ class Person
 
 end
 
-demar = Person.new("Demar", emotions)
-
+demar = Person.new("Demar")
+p demar.inspect
 demar.emotion_metre
